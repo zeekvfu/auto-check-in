c27c3067744e59417c83083201f6b984bcf13854
zeekvfu <wzmindlog@gmail.com>
2016-11-27 23:04:59 +0800

    taobao_taojinbi.py, fix bug, refactor code

--- src/taobao_taojinbi.py
+++ src/taobao_taojinbi.py
@@ -12,10 +12,11 @@ from lxml import etree
 
 from selenium.webdriver import Chrome, Firefox
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 
 from util.utility import get_logger
 from util.http_utility import pc_browser_ua
@@ -65,7 +66,7 @@ class TaoBao:
         try:
             self.driver.find_element_by_xpath('//div[@class="login-switch" and @onselectstart="return false;"]/i[@id="J_Quick2Static" and @class="iconfont static"]').click()
         except NoSuchElementException as e:
-            self.logger.debug("%s(): NoSuchElementException" % this_func_name)
+            self.logger.debug("%s(): NoSuchElementException\t%s" % (this_func_name, self.driver.current_url))
 
         self.driver.find_element_by_id("TPL_username_1").clear()
         self.driver.find_element_by_id("TPL_username_1").send_keys(self.user_name)
@@ -78,12 +79,13 @@ class TaoBao:
             source = self.driver.find_element_by_xpath('//span[@id="nc_1_n1z" and @class="nc_iconfont btn_slide" and @style]')
             ActionChains(self.driver).drag_and_drop_by_offset(source, 600, 0).perform()
         except NoSuchElementException as e:
-            self.logger.debug("%s(): NoSuchElementException" % this_func_name)
+            self.logger.debug("%s(): NoSuchElementException\t%s" % (this_func_name, self.driver.current_url))
 
         flag = False
         if '我的足迹' in self.driver.page_source:
             flag = True
         self.logger.debug("%s(): login: %s" % (this_func_name, flag))
+        self.logger.debug("%s(): end ..." % this_func_name)
         return flag
 
 
@@ -112,6 +114,7 @@ class TaoBao:
         urls = tree.xpath('//div[@class="content"]/div[@class="item"]/a[@class="item-link"]/@href')
         urls = [ 'https:' + url for url in  urls ]
         self.logger.debug("%s(): urls\t%s" % (this_func_name, urls))
+        self.logger.debug("%s(): end ..." % this_func_name)
         return urls
 
 
@@ -127,19 +130,25 @@ class TaoBao:
             time.sleep(5)
             self.driver.find_element_by_xpath('//a[@href="#" and @class="now-take J_NowSignIn" and text()="立即签到"]').click()
             time.sleep(3)
+        self.logger.debug("%s(): end ..." % this_func_name)
+        return
 
 
     # 收藏店铺
     def favorite_shop(self):
         this_func_name = sys._getframe().f_code.co_name
         self.logger.debug("%s(): current_url: %s" % (this_func_name, self.driver.current_url))
-        self.driver.find_element_by_xpath('//p[@class="btns"]/button[@type="submit" and @class="bt-submit"]').click()
-        time.sleep(2)
         # 领取的淘金币可能超过每日上限
         flag = True
-        if '您今天获得的淘金币将达上限，本次领取失败' in self.driver.page_source:
-            flag = False
+        try:
+            self.driver.find_element_by_xpath('//p[@class="btns"]/button[@type="submit" and @class="bt-submit"]').click()
+            time.sleep(2)
+            if '您今天获得的淘金币将达上限，本次领取失败' in self.driver.page_source:
+                flag = False
+        except NoSuchElementException as e:
+            self.logger.debug("%s(): NoSuchElementException\t%s" % (this_func_name, self.driver.current_url))
         self.logger.debug("%s(): current_url: %s\tflag: %s" % (this_func_name, self.driver.current_url, flag))
+        self.logger.debug("%s(): end ..." % this_func_name)
         return flag
 
 
@@ -165,10 +174,10 @@ class TaoBao:
             if '一直蒙头分享啊?歇歇,去' in self.driver.page_source or '分享成功！今天分享赚金币将超上限，去花金币抵钱~' in self.driver.page_source:
                 flag = False
         except NoSuchElementException as e:
-            self.logger.debug("%s(): NoSuchElementException" % this_func_name)
-        finally:
-            self.logger.debug("%s(): current_url: %s\tflag: %s" % (this_func_name, self.driver.current_url, flag))
-            return flag
+            self.logger.debug("%s(): NoSuchElementException\t%s" % (this_func_name, self.driver.current_url))
+        self.logger.debug("%s(): current_url: %s\tflag: %s" % (this_func_name, self.driver.current_url, flag))
+        self.logger.debug("%s(): end ..." % this_func_name)
+        return flag
 
 
     # 在 luck4ever.net 上领取淘金币
@@ -176,11 +185,16 @@ class TaoBao:
         this_func_name = sys._getframe().f_code.co_name
         self.logger.debug("%s(): start ..." % this_func_name)
         self.driver.get(self.luck4ever_taojinbi_url)
+        time.sleep(5)
 
         # 喜欢宝贝/收藏店铺
-        elements = self.driver.find_elements_by_xpath('//p[@style="white-space: normal;" and contains(text(), "喜欢宝贝/")]/a[@target="_blank" and contains(@href, "https://favorite.taobao.com/popup/") and @rel="nofollow" and position()<3]')
+        elements = self.driver.find_elements_by_xpath('//p[@style="white-space: normal;" and contains(text(), "喜欢宝贝/")]/a[@target="_blank" and contains(@href, "https://favorite.taobao.com/popup/") and @rel="nofollow"]')
         for element in elements:
-            element.click()
+            try:
+                element.click()
+            except WebDriverException as e:
+                self.logger.debug("%s(): WebDriverException\t%s" % (this_func_name, self.driver.current_url))
+                continue
             # 等待新的 tab 被加载
             time.sleep(5)
             # 切换到新的 window 下
@@ -196,7 +210,11 @@ class TaoBao:
         # 分享店铺（每天的分享次数有限制）
         elements = self.driver.find_elements_by_xpath('//p[@style="white-space: normal;" and contains(text(), "分享店铺:")]/a[@target="_blank" and contains(@href, "https://share.jianghu.taobao.com/share/") and @rel="nofollow"]')
         for element in elements:
-            element.click()
+            try:
+                element.click()
+            except WebDriverException as e:
+                self.logger.debug("%s(): WebDriverException\t%s" % (this_func_name, self.driver.current_url))
+                continue
             # 等待新的 tab 被加载
             time.sleep(5)
             # 切换到新的 window 下
@@ -209,6 +227,7 @@ class TaoBao:
             if not flag:
                 break
 
+        self.logger.debug("%s(): end ..." % this_func_name)
         return
 
 
@@ -218,6 +237,7 @@ class TaoBao:
         self.logger.debug("%s(): start ..." % this_func_name)
         self.driver.get(self.wz598_taojinbi_url)
         time.sleep(10)
+        self.logger.debug("%s(): end ..." % this_func_name)
         return
 
 
@@ -230,6 +250,7 @@ class TaoBao:
             self.luck4ever_taojinbi()
             self.wz598_check_in()
         self.driver.quit()
+        self.logger.debug("%s(): end ..." % this_func_name)
         return
 
 
