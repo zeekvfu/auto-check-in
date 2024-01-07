9209a0040ce6fdda1900bcd4bfeccdbeaa4287ba
zeekvfu <wzmindlog@gmail.com>
2024-01-08 02:09:25 +0800

    add `python/rq.py` & refactor code

---
+++ run/auto_check_in.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# auto_check_in.sh
+
+
+work_dir=$(cd "$(dirname "$0")"; pwd)
+cd "${work_dir}"
+source "${work_dir}/util.sh"
+
+
+if [ $# -ne 1 ]; then
+	echo "CMD: $0 app"
+	exit 1
+fi
+
+
+MyProject=`get_my_project_path`
+AutoCheckIn="$MyProject/auto-check-in"
+PythonUtilSrc="$MyProject/python-util/src"
+export PYTHONPATH=$PYTHONPATH:$PythonUtilSrc
+
+
+rand=$(( ( RANDOM % 3600 ) ))
+sleep ${rand}s
+
+
+app_file_name="$1".py
+cd $AutoCheckIn/python
+python3.9 ${app_file_name}
+
+
+exit 0
+
+
